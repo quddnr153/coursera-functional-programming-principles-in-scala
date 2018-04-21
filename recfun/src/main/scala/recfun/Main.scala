194cc7d4059aeac8c7309b4b8e08622e5169580b
Byungwook Lee <quddnr153@gmail.com>
2018-04-21 12:04:32 +0900

    recursion - parentheses balancing using tail recursion

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -13,40 +15,42 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = {
-      if (c == 0 || c == r) return 1
-      pascal(c - 1, r - 1) + pascal(c, r - 1)
-    }
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) return 1
+    pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = {
-      val parentheses = chars.toStream.filter(c => c == '(' || c == ')').toList
+  def balance(chars: List[Char]): Boolean = {
+    val INITIAL_OPEN_COUNT = 0
+    val parentheses = chars filter (c => c == '(' || c == ')')
 
-      def newCount(openCount: Int, char: Char): Int = {
-        if (char == '(') openCount + 1 else if (char == ')') openCount - 1 else openCount
-      }
-
-      def balanceWithCount(parentheses: List[Char], openCount: Int): Boolean = {
-        if (parentheses.isEmpty) openCount == 0
-        else if (openCount < 0) false
-        else balanceWithCount(parentheses.tail, newCount(openCount, parentheses.head))
-      }
-
-      balanceWithCount(parentheses, 0)
+    def calculateOpens(numberOfOpen: Int, char: Char): Int = {
+      if (char == '(') numberOfOpen + 1 else if (char == ')') numberOfOpen - 1 else numberOfOpen
     }
+
+    @tailrec
+    def iterate(parentheses: List[Char], numberOfOpen: Int): Boolean = {
+      if (parentheses.isEmpty) numberOfOpen == 0
+      else if (numberOfOpen < 0) false
+      else iterate(parentheses.tail, calculateOpens(numberOfOpen, parentheses.head))
+    }
+
+    iterate(parentheses, INITIAL_OPEN_COUNT)
+  }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = {
-      def count(money: Int, sortedCoins: List[Int]) : Int = {
-        if (sortedCoins.isEmpty) 0
-        else if (money - sortedCoins.head == 0) 1
-        else if (money - sortedCoins.head < 0) 0
-        else countChange(money - sortedCoins.head, sortedCoins) + countChange(money, sortedCoins.tail)
-      }
-      count(money, coins.sorted)
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def count(money: Int, sortedCoins: List[Int]) : Int = {
+      if (sortedCoins.isEmpty) 0
+      else if (money - sortedCoins.head == 0) 1
+      else if (money - sortedCoins.head < 0) 0
+      else countChange(money - sortedCoins.head, sortedCoins) + countChange(money, sortedCoins.tail)
     }
+    count(money, coins.sorted)
   }
+}
