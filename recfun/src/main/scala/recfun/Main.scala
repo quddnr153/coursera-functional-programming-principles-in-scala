a867d99c6efa29f6501b10dd46612a8e903f2036
Byungwook Lee <quddnr153@gmail.com>
2018-04-21 12:43:27 +0900

    count change

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -27,8 +27,9 @@ object Main {
     val INITIAL_OPEN_COUNT = 0
     val parentheses = chars filter (c => c == '(' || c == ')')
 
-    def calculateOpens(numberOfOpen: Int, char: Char): Int = {
-      if (char == '(') numberOfOpen + 1 else if (char == ')') numberOfOpen - 1 else numberOfOpen
+    def calculateOpens(numberOfOpen: Int, char: Char): Int = char match {
+      case '(' => numberOfOpen + 1
+      case ')' => numberOfOpen - 1
     }
 
     @tailrec
@@ -45,12 +46,8 @@ object Main {
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    def count(money: Int, sortedCoins: List[Int]) : Int = {
-      if (sortedCoins.isEmpty) 0
-      else if (money - sortedCoins.head == 0) 1
-      else if (money - sortedCoins.head < 0) 0
-      else countChange(money - sortedCoins.head, sortedCoins) + countChange(money, sortedCoins.tail)
-    }
-    count(money, coins.sorted)
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
